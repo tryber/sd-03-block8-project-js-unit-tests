4ecaaa377404f9f7a2db6a1baa48da48cb53e9ee
Rodrigo Brezolin <brezolinr@gmail.com>
2020-03-24 19:17:49 -0300

    exercicio3 - VQV

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+
+  console.log(`Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`);
+
+};
+
+vqv('Tunico', 30);
 
 module.exports = vqv;
