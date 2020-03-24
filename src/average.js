291eeb98135c45a5697aafda4ab41b94eea18495
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-24 19:28:31 -0300

    average function

--- src/average.js
+++ src/average.js
@@ -14,10 +14,11 @@
 
 const average = (arr) => {
   let sum = 0;
-  if(arr.length === 0) return undefined;
+  if (arr.length === 0) {
+    return undefined;
+  }
   for (let i = 0; i < arr.length; i += 1) {
-    if (typeof arr[i] !== 'number')
-    {
+    if (typeof arr[i] !== "number") {
       return undefined;
     }
     sum += arr[i];
