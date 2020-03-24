744b1e24a9a66574954f7833bc319da492441455
wendersonbelko <contato@wendersonbelko.com>
2020-03-24 20:23:04 -0300

    lobo solitario

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (array) => {
+  return {
+    nome: `${array}, o Lobo Solitário`,
+    feedback: () => 'Eita pessoa boa!',
+  }
+};
 
 module.exports = createStudent;
