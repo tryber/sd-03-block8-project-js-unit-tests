717477ee86094dd371e779e27611fbbd6d511504
alcantara2509 <alcantara2509@gmail.com>
2020-03-24 19:34:10 -0300

    alcantara2509

--- src/average.js
+++ src/average.js
@@ -12,6 +12,6 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => Math.round(arr.reduce((a, b) => a + b) / arr.length);
 
 module.exports = average;
