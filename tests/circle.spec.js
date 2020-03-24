0d4a780daa861dd4c40e68a250b38632ed2cbf04
wendersonbelko <contato@wendersonbelko.com>
2020-03-24 19:05:45 -0300

    nao ta dando nota

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -23,8 +23,8 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente. a
 */
 
-describe('#circle', (parametro) => {
-  it('given a radius, should return an object with circles info', (parametro) => {
+describe('#circle', () => {
+  it('given a radius, should return an object with circles info', () => {
     
   });
 });
