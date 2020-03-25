d0b754cc271cbce1290384698afe0d03259bbfe2
Marylange <marylangess@gmail.com>
2020-03-25 14:07:23 -0300

    Verificando o primeiro teste

--- src/average.js
+++ src/average.js
@@ -11,7 +11,36 @@
     - average([1, 2]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
+const assert = require('assert');
 
-const average = () => {};
+const average = (arr) => {
+
+
+
+
+  let soma = 0;
+  let divisao = 0;
+  for (let i = 0; i < arr.length; i++) {
+    soma += arr[i];
+  }
+
+  divisao = soma / arr.length;
+  return divisao;
+};
+
+let saida = average([10, 10, 10]);
+assert.strictEqual(10, saida);
+
+saida = average("string");
+assert.strictEqual(undefined, saida);
+
+saida = average([]);
+assert.strictEqual(0, saida);
+
+saida = average([10, "1", 10]);
+assert.strictEqual(undefined, saida);
+
+assert.strictEqual(typeof average, 'function');
+
 
 module.exports = average;
