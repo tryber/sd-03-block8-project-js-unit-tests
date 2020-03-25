85a4e9abbe502c79af0b85ccd0f7e9383c381b71
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-03-25 16:05:05 -0300

    Adicionando os tests do arquivo productDetails.spec.js

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,16 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(Array.isArray(productDetails()), true);
+    assert.strictEqual(productDetails().length, 2);
+    assert.strictEqual(typeof productDetails(), 'object');
+    assert.notDeepStrictEqual(
+      Object.entries(productDetails('produto1', 'produto2')[0]),
+      Object.entries(productDetails('firstProduct', 'secondProduct')[1])
+    );
+    assert.strictEqual(
+      Object.values(productDetails('Morango', 'Suspiro'))[0].details.productId.endsWith(123),
+      Object.values(productDetails('Morango', 'Suspiro'))[1].details.productId.endsWith(123)
+    );
   });
 });
