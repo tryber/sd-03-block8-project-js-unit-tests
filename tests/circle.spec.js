7db68dd1d313a4aac193ab836decfce31a6c37d7
LulDelRey <rpgstrikered@gmail.com>
2020-03-25 16:09:41 -0300

    code climate and jest test

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -26,8 +26,9 @@ const circle = require('../src/circle');
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
-    assert.strictEqual(typeof circle, 'object');
+    assert.strictEqual(typeof (circle(1)), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual((Object.keys(circle(3)).length), 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
