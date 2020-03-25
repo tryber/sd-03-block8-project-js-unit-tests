5d995ff874e25a3ee14a62ad022d1c0b90689eaf
Clayton Pereira <cpereiramt@gmail.com>
2020-03-25 16:51:01 -0400

    Salvando a estrutura inicial do projeto

--- src/average.js
+++ src/average.js
@@ -12,6 +12,10 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  if (arr.length === 0 ) {
+    return undefined;   
+  }
+};
 
 module.exports = average;
