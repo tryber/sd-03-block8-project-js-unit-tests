9e8aeb7db79cea5c8ed76fc266fe236104451efc
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-03-25 18:28:57 -0300

    Att

--- src/vqv.js
+++ src/vqv.js
@@ -18,7 +18,10 @@
 
 const vqv = (nome, idade) => {
   if (nome && idade) {
-    return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+    return `Oi, meu nome é ${nome}!\n
+    Tenho ${idade} anos,\n
+    trabalho na Trybe e mando muito em programação!\n
+    #VQV!`;
   }
   return undefined;
 };
