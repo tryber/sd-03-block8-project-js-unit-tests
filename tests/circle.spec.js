5b970896c303f4fda0fb464de03b1bcaea4c55bd
Rhian Castro <rhi.castro@gmail.com>
2020-03-25 15:53:16 -0300

    commit

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,6 +27,7 @@ describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     assert.fail();
     assert.strictEqual(circle(1), {radius:1, area: 3.14, circumference: 6.28});
+    assert.strictEqual(circle(7), {radius:7, area: 153.86, circumference: 43.96});
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
