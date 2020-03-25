cc015d45dc763000d45c46cd8a9eb71640e53309
LulDelRey <rpgstrikered@gmail.com>
2020-03-25 06:55:10 -0300

    code climate2

--- src/average.js
+++ src/average.js
@@ -12,7 +12,7 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = (arrInput) => {  
+const average = (arrInput) => {
   let sum = 0;
   if (!arrInput || arrInput.length === 0) return undefined;
   for (i = 0; i < arrInput.length; i += 1) {
