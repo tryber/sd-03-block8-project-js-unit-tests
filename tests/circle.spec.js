1686034afa8e4c35e71d0fa1e5ef2869dce7108a
Rhian Castro <rhi.castro@gmail.com>
2020-03-25 17:05:35 -0300

    commit

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -30,9 +30,17 @@ describe('#circle', () => {
     // Teste se circle retorna um objeto.
     assert.strictEqual(typeof (circle(1)), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.values(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    const resultCircumference = circle(2);
+    assert.strictEqual(resultCircumference.circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    const resultCircle3 = circle(3);
+    resultCircle3.area = Number(parseFloat(resultCircle3.area).toPrecision(3));
+    assert.strictEqual(resultCircle3.area, 28.3);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.strictEqual(resultCircle3, { radius: 3, area: 28.3, circumference: 18.84 });
   });
 });
