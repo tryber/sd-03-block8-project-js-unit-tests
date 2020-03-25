6df8db7185a380aee8638289385417352662f1f3
Estela Martins <estelaap_martins@hotmail.com>
2020-03-25 16:45:17 -0300

    code climate

--- src/average.js
+++ src/average.js
@@ -23,7 +23,7 @@ const average = (array) => {
   for (let i = 0; i < array.length; i += 1) {
     if (typeof (array[i]) !== 'number') {
       return undefined;
-      /*break;*/
+      /* break;*/
     }
     soma += array[i];
     media = Math.round(soma / array.length);
