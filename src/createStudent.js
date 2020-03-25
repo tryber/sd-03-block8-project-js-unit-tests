1e4e22bffa8c66facfbd5faa8b39b0ffa7a729ab
lcdlima <leticia.duarte.lima@gmail.com>
2020-03-25 19:09:24 -0300

    Adding function to createStudents.js

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (string) => {
+  const answer = Object.create(null);
+  answer.name = string;
+  answer.feedback = () => 'Eita pessoa boa!';
+  return answer;
+};
 
 module.exports = createStudent;
