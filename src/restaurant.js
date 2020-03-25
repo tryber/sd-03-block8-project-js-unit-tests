2ae45995b5666832c488a9906b9a745afdd18515
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-25 19:44:18 -0300

    codeclimate

--- src/restaurant.js
+++ src/restaurant.js
@@ -71,7 +71,7 @@
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 const restaurant = {};
 
-const verifiedElem = (prodPrice, consumpted) => {  
+const verifiedElem = (prodPrice, consumpted) => {
   for (let prodInd = 0; prodInd < prodPrice.length; prodInd += 1) {
     if (prodInd === consumpted) total += prodPrice[prodInd][1];
   }
