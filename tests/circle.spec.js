b01f3d20b6b618e32faa99c4df0dd59d2beccd50
Mauricio Homma <mhhomma@gmail.com>
2020-03-25 20:13:17 -0300

    test2

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,8 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
+    // assert.fail();
+    assert.strictEqual(circle(), undefined);
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
