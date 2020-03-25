659d3a51762f6a4359d94869ccc80e4f37454ca7
Rhian Castro <rhi.castro@gmail.com>
2020-03-25 14:34:30 -0300

    fixing

--- src/vqv.js
+++ src/vqv.js
@@ -16,8 +16,10 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => (!nome || !idade) ? undefined : `Oi, meu nome é ${nome}!
+const vqv = (nome, idade) => {
+  return (!nome || !idade) ? undefined : `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`;
+#VQV!`
+};
 module.exports = vqv;
