70336d1658a533375aea64296ef1a0361cf885c1
felipedoss <felipecaetano25.1996@gmail.com>
2020-03-25 14:10:43 -0300

    test

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -51,7 +51,14 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    assert.strictEqual(createMenu(undefined), undefined);
+    assert.strictEqual(
+      createMenu({
+        food: {},
+        drink: {}
+      }),
+      { fetchMenu: { food: {}, drink: {} } }
+    );
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
     // ```
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
@@ -61,7 +68,7 @@ describe('#createMenu', () => {
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, verifique que 'objetoRetornado.fetchMenu' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
     // const objetoRetornado = createMenu(objetoQualquer);
-    // objetoRetornado.fetchMenu // Retorno: { food: {}, drink: {}}
+    // objetoRetornado.fetchMenu // Retorno: { food: {}, drink: {} }
     // ```
     // Agora faça o TESTE 3 deste arquivo.
     // --------------------------------------------------------------------------------------
