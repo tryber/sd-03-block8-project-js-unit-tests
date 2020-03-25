77590d9153173c60952d17c3b98176566068d369
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-25 15:03:36 -0300

    myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,18 +17,17 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  myArray.push(0);
+  for (var counter = 2; counter <= 3; counter += 1) myArray.push(counter);
+  for (var counter = 1; counter <= 3; counter += 1) myArray.push(counter);
+  for (var counter = 2; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (var counter = 2; counter <= 3; counter += 1) myArray.push(counter);
-    for (var counter = 1; counter <= 3; counter += 1) myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
-      for (var counter = 2; counter <= 3; counter += 1) myArray.push(counter);
-    }
-    for (var counter = 3; counter <= 3; counter += 1) myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) myArray.push(counter); 
+    myArray.push(counter-1);
   }
+  for (var counter = 2; counter <= 3; counter += 1) myArray.push(counter);
+
   return myArray;
-};
+  };
 
 module.exports = myCounter;
