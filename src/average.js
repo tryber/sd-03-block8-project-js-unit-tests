85d1c374a77f34466d398bca0afd81a68eb33d8a
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-03-25 15:58:41 -0300

    resolvendo requesito 1

--- src/average.js
+++ src/average.js
@@ -12,9 +12,26 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  let soma;
-  let media;
+const average = (array) => {
+  let soma = 0;
+  let media = 0;
+  
+  if (array.length === 0) {
+    return undefined;
+  }
+
+    for (let i=0; i<array.length; i++) {
+      if (typeof (array[i]) !== 'number') {
+        return undefined;
+        break;
+      } else {
+          soma += array[i];
+          media = Math.round(soma / array.length);
+      }
+    }
+
+  return media;
+
 };
- 
+
 module.exports = average;
