15c2f0db25d1fa98393e16c292554d0b7eec0b36
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-03-25 16:18:22 -0300

    Requesito 1 codeclimate

--- src/average.js
+++ src/average.js
@@ -15,23 +15,21 @@
 const average = (array) => {
   let soma = 0;
   let media = 0;
-  
+
   if (array.length === 0) {
     return undefined;
   }
 
-    for (let i=0; i<array.length; i++) {
-      if (typeof (array[i]) !== 'number') {
-        return undefined;
-        break;
-      } else {
-          soma += array[i];
-          media = Math.round(soma / array.length);
-      }
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof (array[i]) !== 'number') {
+      return undefined;
+      break;
     }
+    soma += array[i];
+    media = Math.round(soma / array.length);
+  }
 
   return media;
-
 };
 
 module.exports = average;
