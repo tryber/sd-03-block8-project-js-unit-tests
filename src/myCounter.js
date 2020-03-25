5b68e32980ed174c32707851cae13ca951340a4e
tales <talestales@gmail.com>
2020-03-25 14:46:41 -0300

    dsac

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,13 +17,6 @@
 
 const myCounter = () => {
   var myArray = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
-  /*for (let counter = 0; counter <= 3; counter += 1) {
-  //  myArray.push(counter);
-  //  for (counter = 2; counter <= 3; counter += 1) {
-  //    myArray.push(counter);
-  //  }
-  }*/
-
   return myArray;
 };
 
