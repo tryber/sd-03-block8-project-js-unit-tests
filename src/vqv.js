ffa2c6de6b43431aeb8b862fa68d703396bfe89e
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-24 23:00:12 -0300

    codeclimate

--- src/vqv.js
+++ src/vqv.js
@@ -21,9 +21,7 @@ const vqv = (nome, idade) => {
     return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
-  } else {
-    return undefined;
+#VQV!`;
   }
 };
 
