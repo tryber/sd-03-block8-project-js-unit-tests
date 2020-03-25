945109349ebb0e023bf2b8d733b9fabb3a4d5691
Estela Martins <estelaap_martins@hotmail.com>
2020-03-25 20:29:03 -0300

    adicionando creatStudant.js

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,8 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = () => {
+  assert.strictEqual(typeof circle(4), 'object');
+};
 
 module.exports = createStudent;
