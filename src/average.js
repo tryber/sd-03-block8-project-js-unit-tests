f88a08e7d5ea2a4b1c61741f77d62abec509823c
rogerfaustino <roger_faustino@hotmail.com>
2020-03-25 17:53:15 -0300

    Codeclimate issues

--- src/average.js
+++ src/average.js
@@ -40,8 +40,8 @@ const average = (arr) => {
   let sum = 0;
 
   if (arr.length === 0) return undefined;
-  for (let i = 0; i < arr.length; i+=1) {
-    if (typeof arr[i] !== 'number') return undefined
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof arr[i] !== 'number') return undefined;
     sum += arr[i];
     avg = (sum / arr.length);
   }
