8aff7733ccce0c60270848af0f7f76b7801caafc
Thiago Tavares <thiagooo15@gmail.com>
2020-03-25 20:58:59 -0300

    requisito circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -31,7 +31,7 @@ describe('#circle', () => {
     // Teste se o objeto circle tem 3 entradas.
     assert.equal(Object.keys(circle(1)).length,3)
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    assert.strictEqual(circle(), undefined);
+    assert.notStrictEqual(circle(), 'undefined');
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.deepEqual(circle(2), { radius: 2, area: 12.56, circumference: 12.56 });
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
