19ba258e99fff5e6f3bea6bdceb18cdc747d6e1f
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-03-25 12:21:39 -0300

    Adicionando a função createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+let estudante = {}
+const createStudent = (name) => {
+  estudante.name = name;
+  estudante.feedback = () => 'Eita pessoa boa!'
+  return estudante;
+};
 
 module.exports = createStudent;
