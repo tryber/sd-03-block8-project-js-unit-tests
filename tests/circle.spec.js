1fa0d7377b87c42a3f9953f954bdbcba13a77b2b
Hfreitas <hfreitas86@gmail.com>
2020-03-25 16:30:41 -0300

    iniciado implementacao de testes de circle.spec.js

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,7 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();    
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
@@ -33,5 +33,6 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.strictEqual(typeof circle(1), 'object');
   });
 });
