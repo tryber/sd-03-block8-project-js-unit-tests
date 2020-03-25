a8ec26c5a36160491eb2e637b67a6b27b4026c3f
Rodrigo Brezolin <brezolinr@gmail.com>
2020-03-25 16:42:26 -0300

    ex4

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  return objNome = {
+    name: nome,
+    feedback: functionFrase = () => { return 'Eita pessoa boa!'; },
+  };
+};
 
 module.exports = createStudent;
