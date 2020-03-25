98750e517e0f23f4ed673b735f12685e84f95fc9
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-03-25 16:23:10 -0300

    teste

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,6 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle é um objeto.
     assert.strictEqual(typeof circle(1), 'object');    
@@ -34,7 +33,6 @@ describe('#circle', () => {
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.strictEqual(circle(2),)
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
