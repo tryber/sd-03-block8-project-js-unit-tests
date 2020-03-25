684f1efffb35064b557b77792a5411809be08367
wendersonbelko <contato@wendersonbelko.com>
2020-03-25 20:25:15 -0300

    testando sem o else

--- src/vqv.js
+++ src/vqv.js
@@ -17,10 +17,10 @@
 */
 
 const vqv = (nome, idade) => {
-  if (!nome && !idade) {
-    return undefined }
-  else {
-   return  `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
-  };
+  if (nome && idade) {
+    return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  }
+  return undefined;
+};
 }
 module.exports = vqv;
