3210ccc227ac4cd97f559b010eb04761b1da6059
Estela Martins <estelaap_martins@hotmail.com>
2020-03-25 19:03:45 -0300

    testando branch

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -32,7 +32,7 @@ describe('#circle', () => {
     // Teste se o objeto circle tem 3 entradas.
     assert.deepEqual(circle(1), { radius: 1, area: 3.14, circumference: 6.28 });
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    //assert.strictEqual(circle(), undefined);
+    // assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.deepEqual(circle(2), { radius: 2, area: 12.56, circumference: 12.56 });
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
