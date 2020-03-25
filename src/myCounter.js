1b8b7731004919e1180a4ef81c29bccca150b7e5
fernando_luckesi <fernando.luckesi94@gmail.com>
2020-03-25 14:48:37 -0300

    myCounter.spec.js finalizado

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,7 +16,7 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let i = 0; i <= 3; i += 1) {
     myArray.push(i);
     for (let counter = 2; counter <= 3; counter += 1) {
