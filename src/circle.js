c44f00a58d309b445f223b414b1aacb17891ed0a
Raphael Ribeiro dos Santos <raphal.r2021@gmail.com>
2020-03-26 16:22:50 -0300

    80%

--- src/circle.js
+++ src/circle.js
@@ -20,12 +20,12 @@
 
 const circle = (radius) => {
   const PI = 3.14;
-
+  if (!radius) { return undefined; }
   return {
     radius,
     area: PI * radius * radius,
     circumference: 2 * PI * radius,
   };
-};
+}; 
 
 module.exports = circle;
