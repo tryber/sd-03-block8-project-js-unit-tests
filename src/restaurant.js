10159645ac1563dafeb4e9fb57d0b1fe23950c8d
potew <potew0@gmail.com>
2020-03-25 23:09:05 -0300

    Restaurante até passo 5

--- src/restaurant.js
+++ src/restaurant.js
@@ -42,19 +42,26 @@
 
   IMPORTANTE: COMECE PELO TESTE 1 DO ARQUIVO `tests/restaurant.spec.js` E NÃO PELO PASSO 1 DESTE ARQUIVO!
 */
-
+const algo = { 
+  food: {'coxinha': 3.9, 'sopa': 9.9},
+  drink: {'agua': 3.9, 'cerveja': 6.9} 
+};
 // PASSO 1: Crie uma função `createMenu()` que, dado um objeto passado por parâmetro, retorna um objeto com o seguinte formato: { fetchMenu: objetoPassadoPorParametro }.
-//
-// Agora faça o TESTE 2 no arquivo `tests/restaurant.spec.js`.
 
+const createMenu = (objeto) => {
+  return {fetchMenu: objeto};
+}
+// Agora faça o TESTE 2 no arquivo `tests/restaurant.spec.js`.
 //------------------------------------------------------------------------------------------
 
 // PASSO 2: Adicione ao objeto retornado por `createMenu` uma chave `consumption` que, como valor inicial, tem um array vazio.
 //
-// Agora faça o TESTE 5 no arquivo `tests/restaurant.spec.js`.
+  createMenu.consumption = [];
+  console.log(createMenu(algo));
+// Agora faça o TESTE 4 no arquivo `tests/restaurant.spec.js`.
 
 //------------------------------------------------------------------------------------------
-
+                PAREI AQUI.
 // PASSO 3: Crie uma função, separada da função `createMenu()`, que, dada uma string recebida por parâmetro, adiciona essa string ao array de `objetoRetornado.consumption`. Adicione essa função à chave `order`.
 // DICA: para criar isso, você vai precisar definir a função `createMenu()`, definir o objeto que a `createMenu()` define separadamente dela e, depois, a função que será definida em `order`.
 // ```
@@ -70,7 +77,5 @@
 //------------------------------------------------------------------------------------------
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
-
-const createMenu = () => {};
 
 module.exports = createMenu;
