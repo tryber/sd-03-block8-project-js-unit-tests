b3fdd98d3d6f5a3f2c0adb89bab34ed9a66c15c8
luis.campos <gucampos845@gmail.com>
2020-03-26 16:10:21 -0300

    Atualização

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counteri = 2; counteri <= 3; counteri += 1) {
+      myArray.push(counteri);
     }
   }
   return myArray;
