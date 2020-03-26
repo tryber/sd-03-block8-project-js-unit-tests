0882045aefc766889dbee84267ce8eba7db77b3d
PedroParreiras <pedro.bgparreiras@gmail.com>
2020-03-25 22:54:28 -0300

    clean

--- src/average.js
+++ src/average.js
@@ -14,7 +14,7 @@
 const average = (arr) => {
   if (arr.every(torf => typeof torf === 'number')) {
     return Math.round(
-      arr.reduce((a, c) => {a + c}, 0) / arr.length);
+      arr.reduce((a, c) => {return a + c}) / arr.length);
   }
   return average;
 };
