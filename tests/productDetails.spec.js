1f20b913f01a0cd6b38cd4e8fbafa016585bcef6
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-03-25 23:49:15 -0300

    #6 done

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -37,8 +37,14 @@ describe('#productDetails', () => {
     // Teste que o retorno da função é um array.
     assert.strictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails()[0], 'object');
+    assert.strictEqual(typeof productDetails()[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepEqual(productDetails()[0], productDetails()[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails()[0].details.productId.slice(-3), '123');
+    assert.strictEqual(productDetails()[1].details.productId.slice(-3), '123');
   });
 });
