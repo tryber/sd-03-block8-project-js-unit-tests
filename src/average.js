529f5fe572b96e3799b355b040e507afdef5cd71
DiegoRafaelAF <draf0314@gmail.com>
2020-03-26 21:02:59 -0300

    Soluction test average

--- src/average.js
+++ src/average.js
@@ -12,20 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-// const average = (array) => {
-//   let resultado = null;
-//   if (array.length === 0) {
-//     return undefined;
-//   }
+const average = (array) => {
+  let resultado = null;
+  if (array.length === 0) {
+    return undefined;
+  }
   
-//   for (let i = 0; i < array.length; i += 1) {
-//     if (typeof array[i] !== 'number') {
-//       return undefined;
-//     }
-//     resultado += array[i];
-//   }
-//   const media = resultado / array.length;
-//   return Math.round(media);
-// };
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    resultado += array[i];
+  }
+  const media = resultado / array.length;
+  return Math.round(media);
+};
 
 module.exports = average;
