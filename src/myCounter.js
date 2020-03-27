ac9b52a0c03a4d0a1901575313c4a8fe902dfab2
Rodrigo Brezolin <brezolinr@gmail.com>
2020-03-27 12:51:28 -0300

    myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,22 +17,19 @@
 
 const myCounter = () => {
   var myArray = [];
+
   myArray.push(0, 2, 3);
-  for (var counter = 1; counter <= 3; counter += 1) {
-    myArray.push(counter);
-  }
-  for (var counter = 2; counter <= 3; counter += 1) {
-    myArray.push(counter);
 
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
-      myArray.push(counter - 1);
-    }
-  }
+  for (var counter = 1; counter <= 3; counter += 1) myArray.push(counter);
 
   for (var counter = 2; counter <= 3; counter += 1) {
     myArray.push(counter);
+    for (var counter = 2; counter <= 3; counter += 1) myArray.push(counter);
+    myArray.push(counter - 1);
   }
+  
+  for (var counter = 2; counter <= 3; counter += 1) myArray.push(counter);
+
   return myArray;
 };
 
