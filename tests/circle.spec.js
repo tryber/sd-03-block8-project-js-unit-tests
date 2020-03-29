1e3a8ca166fad8d6ac555763871aa8055e7ae8c3
Ga Pimentel <cadgap@gmail.com>
2020-03-29 13:52:47 -0300

    testanto o item que não estava dando certo do circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -30,15 +30,19 @@ describe('#circle', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle é um objeto.
     assert.strictEqual(typeof circle(2), 'object');
+
     // Teste se o objeto circle tem 3 entradas.
     assert.strictEqual(Object.keys(circle(2)).length, 3);
+
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    //assert.strictEqual(circle(), undefined);
+    assert.strictEqual(circle(), undefined);
+
     // Teste que a função retorna, dentro de um objeto, a circunferência' correta para um círculo de raio 2.
-    
     assert.strictEqual(parseFloat(circle(2).circumference.toPrecision(4)), 12.56);
+
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     assert.strictEqual(parseFloat(circle(3).area.toPrecision(4)), 28.26);
+    
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     let objCircle = circle(3);
     let resultCircle = {
