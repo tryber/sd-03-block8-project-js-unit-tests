3bec4d5fd88c6effeebff1db301985d41b94aaf1
saturnian99 <saturnian.dsg@gmail.com>
2020-03-29 18:56:55 -0300

    average.js CC fixing issues

--- src/average.js
+++ src/average.js
@@ -16,13 +16,13 @@ const average = (array) => {
   let sum = 0;
   let avg = 0;
 
-  if (array.length === 0) { return undefined; } 
-  
-  for (i in array) {
-    if (typeof array[i] !== "number") { return undefined; }
-    else { sum += array[i]; }
+  if (array.length === 0) { return undefined; }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') { return undefined; }
+    sum += array[i];
   }
-  
+
   avg = sum / array.length;
   return Math.round(avg);
 };
