1c596f793c88586e1f825842adbea650763d0810
Gabriel Oliveira Ferreira <gabriel1335555@gmail.com>
2020-03-29 17:05:37 -0300

    codeclimate2

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,7 +15,7 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => ({
+const createStudent = name => ({
   name: `${name}`,
   feedback: () => 'Eita pessoa boa!',
 });
