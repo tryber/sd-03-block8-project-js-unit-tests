9c403f5899cd0076861418bad520ea42f3d0d63c
Marylange <marylangess@gmail.com>
2020-03-29 15:52:33 -0300

    createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,12 +14,11 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-const createStudent = name => {
+const createStudent = (name) => {
   return {
-    'name': name,
-    feedback: () => {
-      return 'Eita pessoa boa!';
-    },
+    name: name,
+    feedback: () =>
+       'Eita pessoa boa!'
   };
 };
 // const estudante = createStudent('Leandrão, o Lobo Solitário');
