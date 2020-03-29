dd1da1bfb34cd6fc2409163a9996edb079bd0988
Marylange <marylangess@gmail.com>
2020-03-29 16:20:50 -0300

    createStudent teste3

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 const createStudent = (nameRetorno) => {
-  const coisa = {
+  const funcao = {
     name: nameRetorno,
     feedback: () => 'Eita pessoa boa!',
   };
-  return coisa;
+  return funcao;
 };
 // const estudante = createStudent('Leandrão, o Lobo Solitário');
 module.exports = createStudent;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -52,7 +52,7 @@ const createMenu = require('../src/restaurant');
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
     // const buscarMenu = 'coxinha';
-    // assert.fail();
+    assert.fail();
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
     // assert.strictEqual(typeof createMenu(buscarMenu)[0], 'object');
     // // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
