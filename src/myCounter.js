72198532ebd831522effc7dc8501df87d66d79a0
renan.andreolle <renandreolle@hotmail.com>
2020-03-29 18:08:34 -0300

    counter ajustado2

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let inner = 2; inner <= 3; inner += 1) {
+      myArray.push(inner);
     }
   }
   return myArray;
