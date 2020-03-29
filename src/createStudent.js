cff3fb8dd5b965e922603f3a88add7f218b06a2a
gabryel <gabryel.ryba@gmail.com>
2020-03-29 18:29:02 -0300

    codeclimate

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
-  return {
-    name: `${nome}`,
-    feedback: () => 'Eita pessoa boa!',
-  };
-};
+const createStudent = (nome) => objeto = {name: `${nome}`, feedback: () => 'Eita pessoa boa!',};
 
 module.exports = createStudent;
