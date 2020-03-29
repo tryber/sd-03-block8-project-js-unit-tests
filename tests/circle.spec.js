af7888e2a45e656286c8aae2a9ae781f6ce2f974
lcdlima <leticia.duarte.lima@gmail.com>
2020-03-29 19:07:25 -0300

    Adjusting circle.spec.js after requirements update

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -31,7 +31,7 @@ describe('#circle', () => {
     // Teste se o objeto retornado tem 3 entradas.
     assert.strictEqual(Object.keys(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    assert.notEqual(circle(), undefined);
+    assert.deepStrictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(Object.values(circle(2))[2], 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
