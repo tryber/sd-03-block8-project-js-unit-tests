e1b6dc17f97b676201ebf762c58fc066ab7e16f4
Clayton Pereira <cpereiramt@gmail.com>
2020-03-28 20:33:25 -0400

    Salvando o caso de uso restaurant.spec.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -71,26 +71,25 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-//const restaurant = {fetchMenu:{food:{},drinks:{},consumption: [],},};
+// const restaurant = {fetchMenu:{food:{},drinks:{},consumption: [],},};
 const createMenu = restaurant => (
   restaurant = { fetchMenu: {
     food: { coxinha: 3.90, sanduiche: 9.90 },
-    drinks: { agua: 3.90, cerveja: 6.90 }},
+    drinks: { agua: 3.90, cerveja: 6.90 } },
     consumption: [],
     order: (request) => {
-      restaurant.consumption = request;
-     
-    return restaurant.consumption;
+      restaurant.consumption = request;     
+      return restaurant.consumption;
     },
     pay: () => {
-      let price =0;
+      let price = 0;
       const reducer = (accumulator, currentValue) => accumulator + currentValue;
-      price += Number(Object.values(restaurant.fetchMenu.food).reduce(reducer)); 
+      price += Number(Object.values(restaurant.fetchMenu.food).reduce(reducer));
       price += Number(Object.values(restaurant.fetchMenu.drinks).reduce(reducer));
-      price += Math.round((price/100) * 10);
+      price += Math.round((price / 100) * 10);
       return price;
     },
-  });  
+  });
 /* const orderFromMenu = (request) => {
   restaurant.consumption = restaurant.order(request);
 } */
