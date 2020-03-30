ab9700ce3e8510dd44cc8c134fd4b0cf4cd9bda5
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-30 04:14:56 -0300

    master

--- src/restaurant.js
+++ src/restaurant.js
@@ -71,30 +71,29 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const objetoQualquer = { food: { coxinha: 3.90, sanduiche: 9.90 }, drink: { agua: 3.90, cerveja: 6.90 } };
 let restaurant = {};
 const orderFromMenu = request => restaurant.consumption.push(request);
 const createMenu = objetoQualquer => ({
-fetchMenu: objetoQualquer,
-consumption: [],
-order: orderFromMenu,
-  pay () {
-    let pagar = 0;
-    this.consumption.forEach(elemento => {
-       this.fetchMenu.food[elemento] ? pagar += this.fetchMenu.food[elemento] : pagar += this.fetchMenu.drink[elemento];
-    })
-    return pagar*1.1
-  }
+  fetchMenu: objetoQualquer,
+  consumption: [],
+  order: orderFromMenu,
+    pay () {
+      let pagar = 0;
+      this.consumption.forEach(elemento => {
+         this.fetchMenu.food[elemento] ? pagar += this.fetchMenu.food[elemento] : pagar += this.fetchMenu.drink[elemento];
+      });
+      return pagar * 1.1;
+    }
 });
-restaurant = createMenu(objetoQualquer);
-//restaurant.order('coxinha')
-//restaurant.order("coxinha");
-//restaurant.order("agua");
-//restaurant.order('coxinha');
-//restaurant.order('agua');
-//restaurant.order('coxinha');
-//console.log(restaurant.consumption)
-//console.log(restaurant.pay())
+restaurant = createMenu({ food: { coxinha: 3.90, sanduiche: 9.90 }, drink: { agua: 3.90, cerveja: 6.90 } });
+restaurant.order('coxinha')
+restaurant.order("coxinha");
+restaurant.order("agua");
+restaurant.order('coxinha');
+restaurant.order('agua');
+restaurant.order('coxinha');
+console.log(restaurant.consumption)
+console.log(restaurant.pay())
 
 module.exports = createMenu;
 
