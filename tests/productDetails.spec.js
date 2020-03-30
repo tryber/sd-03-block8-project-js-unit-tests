f63123d7130c011247ffb0bbe0c967898a29bfca
DiegoRafaelAF <draf0314@gmail.com>
2020-03-30 11:59:36 -0300

    Soluction test ProductDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,12 +36,15 @@ describe('#productDetails', () => {
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.ok(productDetails(), []);
+    assert.notEqual(typeof productDetails(), 'array');
     // Teste que o array retornado pela função contém dois itens dentro.
-    
+    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails('a', 'b'), 'object')
     // Teste que os dois objetos são diferentes entre si.
-    
+    assert.notEqual(productDetails('a')[0], productDetails('b')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails()[0].details.productId.slice(-3), '123');
+    assert.strictEqual(productDetails()[1].details.productId.slice(-3), '123');
   });
 });
