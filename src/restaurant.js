e87005a8b0c2428e7351e0172348553af7ca7650
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2020-03-31 01:25:24 -0300

    testando

--- src/restaurant.js
+++ src/restaurant.js
@@ -6,40 +6,26 @@
   - fazer pedidos;
   - verificar o que foi pedido;
   - somar o valor da conta.
-
   A estrutura deste código e deste objeto já foi definida e você irá implementá-la.
   Abaixo você verá uma série de testes e passos que devem ser, NECESSARIAMENTE, feitos em ordem para o bom desenvolvimento do sistema. Eles guiarão você pelo desenvolvimento.
-
   Parâmetros:
   - Um objeto. Exemplos: { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} }.
   Comportamento:
-
   const meuRestaurante = createMenu({ food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} }).
-
   meuRestaurante.fetchMenu() // Retorno: { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} }
-
   meuRestaurante.order('coxinha') // Retorno: undefined
-
   meuRestaurante.consumption // Retorno: ['coxinha']
-
   meuRestaurante.pay() // Retorno: 3.9
-
   Uma função createMenu retorna um objeto com as seguintes características:
   - Uma chave `fetchMenu` retorna o objeto que a função `createMenu` recebe por parâmetro. O menu tem sempre duas chaves, `food` e `drink`, no seguinte formato:
-
   const meuRestaurante = createMenu({
     food: {'coxinha': 3.90, 'sanduiche', 9.90},
     drinks: {'agua': 3.90, 'cerveja': 6.90}
   });
-
   meuRestaurante.fetchMenu() // Retorno: Menu acima
-
   - Uma chave `consumption` que contém um array de strings, com cada string sendo a chave de um pedido. Por exemplo: ['coxinha', 'cerveja']
-
   - Uma chave `order` que tem uma função que, recebida uma string como parâmetro, adiciona essa string à lista salva em `consumption`.
-
   - Uma chave `pay` que, quando chamada, invoca uma função que soma o valor de todos os pedidos e dá o preço com acréscimo de 10%.
-
   IMPORTANTE: COMECE PELO TESTE 1 DO ARQUIVO `tests/restaurant.spec.js` E NÃO PELO PASSO 1 DESTE ARQUIVO!
 */
 
@@ -70,7 +56,69 @@
 //------------------------------------------------------------------------------------------
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
+let menu = {};
 
-const createMenu = () => {};
+const toArray = (objx, keyx) => {
+  const result = [];
+  for (let i = 0; i < keyx.length; i += 1) {
+    result.push(objx[keyx[i]]);
+  }
+  return result;
+};
+
+const products = (arg1, arg2) => {
+  let productsList = [];
+  const pnf = {};
+
+  productsList = toArray(arg1, arg2);
+
+  for (let i = 0; i < productsList.length; i += 1) {
+    const pn = Object.keys(productsList[i]);
+    const pk = Object.values(productsList[i]);
+    for (let j = 0; j < pn.length; j += 1) {
+      pnf[pn[j]] = pk[j];
+    }
+  }
+  return pnf;
+};
+
+const calculaConta = (pk, pv, cm) => {
+  let result = 0;
+
+  for (let k = 0; k < cm.length; k += 1) {
+    const j = pk.indexOf(cm[k]);
+    if (pk[j] === cm[k]) {
+      result += pv[j];
+    }
+  }
+  return result;
+};
+
+const pagamento = (pm, cm) => {
+  let conta = 0;
+  const produtos = pm;
+  const produtosKeys = Object.keys(produtos);
+  const produtosValues = Object.values(produtos);
+  const consumo = cm;
+  conta = calculaConta(produtosKeys, produtosValues, consumo);
+  conta = parseFloat((conta + (conta * 0.1)).toPrecision(10));
+  return conta;
+};
+
+const createMenu = (obj) => {
+  menu = {
+    fetchMenu: () => obj,
+    comsuption: [],
+    order: (obj1) => { menu.comsuption.push(obj1); },
+    pay: () => {
+      const lista = menu.fetchMenu();
+      const listaKeys = Object.keys(lista);
+      const consumo = menu.comsuption;
+      const produtosMenu = products(lista, listaKeys);
+      return pagamento(produtosMenu, consumo);
+    },
+  };
+  return menu;
+};
 
 module.exports = createMenu;
