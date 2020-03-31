6612becc1e1b8076ec2007d72bab746ec7e60f97
Lourenço <loulage@gmail.com>
2020-03-31 17:37:58 -0300

    Circle fixed 02

--- src/circle.js
+++ src/circle.js
@@ -19,13 +19,13 @@
 */
 
 const circle = (radius) => {
-  const PI = 3.14;
-
-  return {
-    radius,
-    area: PI * radius * radius,
-    circumference: 2 * PI * radius,
-  };
+    const PI = 3.14;
+    if (!radius) { return undefined; }
+    return {
+        radius,
+        area: PI * radius * radius,
+        circumference: 2 * PI * radius,
+    };
 };
 
 module.exports = circle;
