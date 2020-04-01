4e6c9a52b340663ccfc41aa5bcdc50e6eda2782b
Ga Pimentel <cadgap@gmail.com>
2020-04-01 13:53:35 -0300

    ajuste

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,9 +17,14 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 2; counter <= 3; counter -= 1) {
+  var myArray = [];
+  for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
+    for (var counter = 2; counter <= 3; counter += 1) {
+      myArray.push(counter);
+    }
   }
+  return myArray;
   return myArray;
 };
 
