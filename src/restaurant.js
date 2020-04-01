48d4efd265181f80d588907be2bdfd1641c1702b
William Igor <williamigorsilva@gmail.com>
2020-03-31 21:10:09 -0300

    Alterando chaves na function

--- src/restaurant.js
+++ src/restaurant.js
@@ -66,14 +66,12 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = restaurant => 
+const createMenu = restaurant => {
   restaurant = { fetchMenu: {
     food: { coxinha: 3.90, sanduiche: 9.90 },
     drinks: { agua: 3.90, cerveja: 6.90 } },
     consumption: [],
-    order: (request) => {
-      restaurant.consumption = request;
-      return restaurant.consumption;
-    });
+    order: (request) => restaurant.consumption = request
+};
 
 module.exports = createMenu;
