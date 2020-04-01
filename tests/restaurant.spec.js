8e0f827210c48929df8d04e9138683d7f6b21b14
Ga Pimentel <cadgap@gmail.com>
2020-04-01 13:16:55 -0300

    Ajuste assert

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -53,7 +53,7 @@ describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
     // assert.fail();
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
-    assert.deepEqual(createMenu({ food: {}, drink: {} }), {
+    assert.Equal(createMenu({ food: {}, drink: {} }), {
       fetchMenu: {
         food: {}, drink: {}
       }
