6d93ff39f79e80eb903ad70fa01cd0e206cb6624
Ga Pimentel <cadgap@gmail.com>
2020-04-01 12:29:04 -0300

    Início do restaurant

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,11 +17,8 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (var counter = 2; counter <= 3; counter -= 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
-    }
   }
   return myArray;
 };
