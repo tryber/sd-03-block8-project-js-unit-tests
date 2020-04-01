4d7bfee0dbd15965852530c4e8a798d05391873f
Ga Pimentel <cadgap@gmail.com>
2020-04-01 12:36:03 -0300

    Ajustes Climate

--- src/restaurant.js
+++ src/restaurant.js
@@ -72,18 +72,16 @@
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 const restaurant = {};
 console.log(restaurant);
-const createMenu = objetoQualquer => {
-  restaurant = { fetchMenu: objetoQualquer };
+const createMenu = (objetoQualquer) => {
+  restaurant += { fetchMenu: objetoQualquer };
   return restaurant;
-}
+};
 
 restaurant.consumption = [];
-console.log(restaurant);
-const orderFromMenu = itemKey => {
+
+const orderFromMenu = (itemKey) => {
   restaurant.consumption = [itemKey];
-  
   return restaurant;
-}
-console.log(restaurant);
+};
 
 module.exports = createMenu;
