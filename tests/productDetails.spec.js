9f061c5308772986c245bc3c570bf1ffdf6eb535
Rafaopb <59584187+Rafaopb@users.noreply.github.com>
2020-04-01 11:13:32 -0300

    Mudei linha 39

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,7 +36,7 @@ describe('#productDetails', () => {
     //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.notEqual(typeof productDetails(), 'array');
+    assert.strictEqual(typeof productDetails(), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(typeof productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
