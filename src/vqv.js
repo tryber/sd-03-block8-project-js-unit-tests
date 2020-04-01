ee1f54941b8b2425c6942225437bdedc935d4e17
henrique <hbaetaleite@gmail.com>
2020-03-31 23:39:55 -0300

    Gustavao, William e Rafao sao os caras!

--- src/vqv.js
+++ src/vqv.js
@@ -21,8 +21,9 @@ const vqv = (nome, idade) => {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!
-  tenho 30 anos,
-  trabalho na Trybe e mando muito em Programação!`;
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
