12e5db42ab659fc4721d94ac8ee93cc882063470
André Ferreira <andrefpires1@gmail.com>
2020-04-02 16:59:10 -0300

    Mudando função everage

--- src/average.js
+++ src/average.js
@@ -13,18 +13,18 @@
 */
 
 const average = (array) => {
+  let resultado = null;
   if (array.length === 0) {
     return undefined;
   }
-  let soma = 0;
   for (let i = 0; i < array.length; i += 1) {
-    if (typeof array[i] === 'number') {
-      soma += array[i];
+    if (typeof array[i] !== 'number') {
+      return undefined;
     }
-    const resultado = soma / array.length;
-    return resultado;
+    resultado += array[i];
   }
-  return undefined;
+  const media = resultado / array.length;
+  return Math.round(media);
 };
 
 module.exports = average;
