d5bb80574bb9a0f0ef05930165406d03ccced800
André Ferreira <andrefpires1@gmail.com>
2020-04-02 23:32:42 -0300

    Requisito 7**

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -6,11 +6,9 @@ const productDetails = require('../src/productDetails');
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
-
   Parâmetros:
   - Uma string;
   - Uma string;
-
   Comportamento:
   productDetails('Alcool gel', 'Máscara') // Retorna:
   [
@@ -27,18 +25,21 @@ const productDetails = require('../src/productDetails');
       }
     }
   ]
-
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.equal(productDetails[' '],productDetails[' ']);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails.length,(2));
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails(), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(productDetails['0'],productDetails['1']);
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
 });
