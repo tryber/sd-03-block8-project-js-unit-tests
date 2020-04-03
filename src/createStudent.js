91fbb6b1a1b8a6d099b104aa06f9438d81aef717
André Ferreira <andrefpires1@gmail.com>
2020-04-02 23:14:46 -0300

    code climate

--- src/createStudent.js
+++ src/createStudent.js
@@ -1,5 +1,4 @@
- 
-/*
+ /*
   Dada uma função chamada createStudent que recebe como parâmetro um nome,
   retorne um objeto que contenha duas chaves:
     (1) name, contendo o nome passado como parâmetro;
