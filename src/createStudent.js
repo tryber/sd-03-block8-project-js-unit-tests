687548a3f6b4fe19b762734f47cf4d698fa3e656
Cezene <cezenesn@hotmail.com.br>
2020-04-29 17:19:55 -0300

    student

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => { 
+  let student = {name: `${nome}`, feedback: () => { return 'Eita pessoa boa!'} 
+  }
+  return student;
+};
 
 module.exports = createStudent;
