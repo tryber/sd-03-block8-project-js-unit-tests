322a12dae7c5b1493f70c765b17cf327f8007f53
Cezene <cezenesn@hotmail.com.br>
2020-04-29 15:21:02 -0300

    tres

--- src/average.js
+++ src/average.js
@@ -15,7 +15,7 @@
 const average = (numbers) => {
   if (numbers.length <= 0) return undefined;
   let sum = 0;
-  for (let i = 0; i < numbers.length; i +=1) {
+  for (let i = 0; i < numbers.length; i += 1) {
     if (typeof numbers[i] !== 'number') return undefined;
     sum += numbers[i];
   }
