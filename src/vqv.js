4fb66a92d124ef0519a15221559a17b59e513bbd
Cezene <cezenesn@hotmail.com.br>
2020-04-29 17:01:55 -0300

    mais um

--- src/vqv.js
+++ src/vqv.js
@@ -22,7 +22,8 @@ const vqv = (nome, idade) => {
   }
   return `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!`;
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
 };
 
 module.exports = vqv;
