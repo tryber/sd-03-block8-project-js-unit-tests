ab0703f0102483c7839b828de4282a09c40193fb
marcosjuniorclimaco <markojuniorclimako@gmail.com>
2020-05-06 07:13:12 -0300

    continuando projeto

--- src/average.js
+++ src/average.js
@@ -3,7 +3,6 @@
   Caso a função receba algum valor não númerico ou um array vazio,
   o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
-
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
   Comportamento:
@@ -11,22 +10,22 @@
     - average([1, 2]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
+const assert = require('assert');
+// mudança de nome do usuario git
 
 const average = (arr) => {
-  if (arr.length === 0) {
-    return undefined;
-  }
+  if (arr.length === 0) { return undefined; }
+
   for (let i = 0; i < arr.length; i += 1) {
-    if (typeof arr[i] === 'string') {
+    if (typeof arr[i] !== 'number') {
       return undefined;
     }
   }
-  let soma = null;
-  let divisao = null;
-  for (let i = 0; i < arr.length; i += 1) {
-    soma += arr[i];
-    divisao = soma / arr.length;
-  }
-  return Math.round(divisao);
+  const soma = Math.round((arr.reduce((acc, current) => acc + current) / (arr.length)));
+  console.log(soma);
+  return soma;
 };
+assert.deepEqual(average(['a']), undefined);
+assert.deepStrictEqual(average([1, 2, 3, 4, 5]), 3);
+assert.strictEqual(average([1, 3, 'blha']), undefined);
 module.exports = average;
