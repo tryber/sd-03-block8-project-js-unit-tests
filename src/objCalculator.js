70314243b9732d265e0a05b4a5eba50b1cd76d52
marcosjuniorclimaco <markojuniorclimako@gmail.com>
2020-05-06 10:11:51 -0300

    finalizando pj

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -7,18 +7,21 @@
   Para cada uma delas atribua uma função que realiza a respectiva operação.
   A função deve receber dois inteiros e retornar um inteiro.
   Os resultados das divisões devem sempre ser arredondados para baixo.
-
   Faça as funções com arrow functions!
-
   Parâmetros:
   - Um número inteiro;
   - Um número inteiro;
-
   Comportamento:
   calculator.add(1, 1) // Retorno: 2;
   calculator.div(3, 2) // Retorno: 1;
 */
+const assert = require('assert');
 
-const calculator = {};
-
+const calculator = {
+  add: (n1, n2) => (!Number.isInteger(n1, n2) ? undefined : Math.floor(n1 + n2)),
+  mult: (n1, n2) => (!Number.isInteger(n1, n2) ? undefined : Math.floor(n1 * n2)),
+  div: (n1, n2) => (!Number.isInteger(n1, n2) ? undefined : Math.floor(n1 / n2)),
+  sub: (n1, n2) => (!Number.isInteger(n1, n2) ? undefined : Math.floor(n1 - n2)),
+};
+assert.equal(calculator.add(1, 2), 3);
 module.exports = calculator;
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -6,11 +6,9 @@ const productDetails = require('../src/productDetails');
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
-
   Parâmetros:
   - Uma string;
   - Uma string;
-
   Comportamento:
   productDetails('Alcool gel', 'Máscara') // Retorna:
   [
@@ -27,18 +25,21 @@ const productDetails = require('../src/productDetails');
       }
     }
   ]
-
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
-
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('bl', 'foo')), true)
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('bl', 'foo').length, 2)    
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails('bl', 'foo')[0] && typeof productDetails('bl', 'foo')[1], 'object')
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(productDetails('bl', 'foo')[1] !== productDetails('bl', 'foo')[0], true)    
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(/123$/.test(Object.values(productDetails('bl', 'foo'))[0].details.productId) && /123$/.test(Object.values(productDetails('bl', 'foo'))[1].details.productId), true)
   });
 });
